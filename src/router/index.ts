f8fc958b937b5b9aea0596954cc7dbc592346552
RiceMarch <1079966197@qq.com>
2021-04-27 22:45:00 +0800

    feat.three init and try

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,8 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/example01",
-    component: () => import("@/views/example01/example01.vue"),
-  },
-  {
-    path: "/example02",
-    component: () => import("@/views/example02/example02.vue"),
+    path: "/three",
+    component: () => import("@/views/three/three.vue"),
   },
   {
     path: "/login",
--- src/views/example01/example01.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1>exmaple01</h1>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style></style>
